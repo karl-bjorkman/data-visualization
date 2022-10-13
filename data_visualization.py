2d522d1549f70acfc8946cd313f887a92767fbba
Karl Bjorkman <karlbjorkman15@gmail.com>
2022-10-13 01:01:05 -0400

    Updates to data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -3,8 +3,22 @@ from matplotlib import pyplot as plt
 x = [0, 2, 3, 4, 5, 6]
 y = [0, 1, 4, 16, 25, 36]
 
+# Plotting green line with circle markers at each x-axis tick
 plt.plot(x, y, color = 'green', marker = 'o')
 plt.show()
 plt.clf()
 
-print("I love Eszter very much. She is super cool.")
+# Zoom in to x or y axes
+plt.axis([0, 15, 25, 75]) # [xmin, xmax, ymin, ymax]
+
+# Labeling Axes
+plt.xlabel('X Label')
+plt.ylabel('Y Label')
+plt.title('Plot Title')
+
+# Subplots
+plt.subplot(subplot_rows, subplot_columns, subplot_index) # CONFUSING! GO BACK AND REVIEW!
+plt.plot(var1, var2)
+plt.show()
+
+plt.subplots_adjust(left, right, bottom, top, wspace, hspace)
