6576d7b65303ce2910849255cce7c1be424c1a50
Karl Bjorkman <karlbjorkman15@gmail.com>
2022-10-13 14:55:00 -0400

    Added all basic line plot commands

--- data_visualization.py
+++ data_visualization.py
@@ -22,3 +22,28 @@ plt.plot(var1, var2)
 plt.show()
 
 plt.subplots_adjust(left, right, bottom, top, wspace, hspace)
+
+plt.plot([0, 1, 2, 3], [1, 4, 9, 16], label = "line name") # Instead of 'legend'
+plt.legend(['label 1', 'label 2']) # Can also make list of legend labels and feed it to .legend()
+
+# Modify Ticks
+ax = plt.subplot() # 'ax' is axes object
+ax.set_xticks([1, 2, 4]) # Shows only these ticks
+ax.set_yticks([1, 50, 100])
+
+# Use special labels
+ax.set_xticklabels()
+ax.set_yticks([0.1, 0.5, 1.0])
+ax.set_yticklabels(['10%', '50%', '100%'])
+
+# Figures
+plt.figure(figsize = (4, 10))
+
+# Clear all existing plots before creating a new one
+plt.close('all')
+
+# Saving a plot for presentation or website, say
+plt.savefig()
+plt.savefig('example.pdf')
+plt.savefig('example2.png')
+plt.savefig('example3.svg')
